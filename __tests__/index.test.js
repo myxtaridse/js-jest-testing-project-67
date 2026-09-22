19cc5d7b75493432d41c23423aba774e925fa792
Ksenia Durkina <sigismund1old@gmail.com>
2026-09-22 10:18:02 +0300

    test: add a test to verify the downloading of images from the page and the verification of changes to the page’s HTML code

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
-import { test, expect, beforeEach } from 'vitest'
+import { test, expect, beforeEach, afterEach } from 'vitest'
 import os from 'os'
-import { mkdtemp, readFile } from 'fs/promises'
+import { access, mkdtemp, readFile, rm, stat } from 'fs/promises'
 import path from 'path'
 import nock from 'nock'
 
@@ -11,7 +11,7 @@ beforeEach(async () => {
   pageDir = await mkdtemp(path.join(os.tmpdir(), 'page-loader-'))
 })
 
-test('page-loader', async () => {
+test('checking the installation of the page from the internet and checking the creation of a file in the specified directory', async () => {
   const filename = 'ru-hexlet-io-courses.html'
   const content = `<div>Page</div>`
 
@@ -19,6 +19,35 @@ test('page-loader', async () => {
     .get('/courses')
     .reply(200, content)
 
-  await expect(pageLoader('https://ru.hexlet.io/courses', pageDir)).resolves.toBe(filename)
+  const resUrl = await pageLoader('https://ru.hexlet.io/courses', pageDir)
+
+  expect(resUrl).toBe(filename)
   await expect(readFile(path.join(pageDir, filename), 'utf-8')).resolves.toBe(content)
+})
+
+test('checking the download of the page and images to the specified directory', async () => {
+  const getFixture = filename => path.join(process.cwd(), '__fixtures__', filename)
+  const getFile = (...filepath) => path.join(pageDir, ...filepath)
+
+  const beforeHTML = await readFile(getFixture('before.html'), 'utf-8')
+  const afterHTML = await readFile(getFixture('after.html'), 'utf-8')
+
+  const filenameLoader = 'ru-hexlet-io-courses.html'
+  const dirnameLoader = 'ru-hexlet-io-courses_files'
+  const imgLoader = 'ru-hexlet-io-assets-professions-nodejs.png'
+  
+  nock('https://ru.hexlet.io')
+    .get('/courses')
+    .reply(200, beforeHTML)
+
+  const resUrl = await pageLoader('https://ru.hexlet.io/courses', pageDir)
+
+  expect(resUrl).toBe(filenameLoader)
+  await expect(readFile(getFile(filenameLoader), 'utf-8')).resolves.toBe(afterHTML)
+  await (expect(stat(getFile(dirnameLoader, imgLoader)))).resolves.toBeDefined()
+})
+
+
+afterEach(async () => {
+  await rm(pageDir, { recursive: true, force: true })
 })
